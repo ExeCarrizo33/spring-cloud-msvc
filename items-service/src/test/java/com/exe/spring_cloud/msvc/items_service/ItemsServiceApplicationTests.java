f36d8c64cbdde1466192995a3388728ce288858e
Exe <exelcarrizo@gmail.com>
2025-03-06 11:52:39 -0300

    Creacion del microservicio items

---
+++ items-service/src/test/java/com/exe/spring_cloud/msvc/items_service/ItemsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exe.spring_cloud.msvc.items_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
