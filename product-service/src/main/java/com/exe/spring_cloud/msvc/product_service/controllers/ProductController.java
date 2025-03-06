0e9652a2eb11add57b0ebc1209abef17aa37aea0
Exe <exelcarrizo@gmail.com>
2025-03-06 10:13:20 -0300

    Controller products y conexion con bd MySql

---
+++ product-service/src/main/java/com/exe/spring_cloud/msvc/product_service/controllers/ProductController.java
@@ -0,0 +1,33 @@
+package com.exe.spring_cloud.msvc.product_service.controllers;
+
+import com.exe.spring_cloud.msvc.product_service.models.Product;
+import com.exe.spring_cloud.msvc.product_service.services.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    @GetMapping
+    public List<Product> findAll() {
+        return productService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> findById(@PathVariable Long id) {
+        return productService.findById(id)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+
+
+}
