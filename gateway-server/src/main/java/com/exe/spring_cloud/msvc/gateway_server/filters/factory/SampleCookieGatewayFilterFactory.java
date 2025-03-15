cfcc9c58526a2b4fda2d814a64c31b9f81554090
Exe <exelcarrizo@gmail.com>
2025-03-15 02:54:41 -0300

    Modificación del Response HTTP en GatewayFilterFactory

--- gateway-server/src/main/java/com/exe/spring_cloud/msvc/gateway_server/filters/factory/SampleCookieGatewayFilterFactory.java
+++ gateway-server/src/main/java/com/exe/spring_cloud/msvc/gateway_server/filters/factory/SampleCookieGatewayFilterFactory.java
@@ -1,13 +1,20 @@
 package com.exe.spring_cloud.msvc.gateway_server.filters.factory;
 
 import lombok.Data;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
+import java.util.Optional;
+import java.util.logging.Logger;
+
 @Component
 public class SampleCookieGatewayFilterFactory extends AbstractGatewayFilterFactory<SampleCookieGatewayFilterFactory.ConfigCookie> {
+
+    private final Logger logger = (Logger) LoggerFactory.getLogger(SampleCookieGatewayFilterFactory.class);
 
     public SampleCookieGatewayFilterFactory() {
         super(ConfigCookie.class);
@@ -16,9 +23,13 @@ public class SampleCookieGatewayFilterFactory extends AbstractGatewayFilterFacto
     @Override
     public GatewayFilter apply(ConfigCookie config) {
         return ((exchange, chain) -> {
+            logger.info("Ejecutando el pre gateway filter factory: " + config.message);
 
             return chain.filter(exchange).then(Mono.fromRunnable(() -> {
-
+                Optional.ofNullable(config.value).ifPresent(cookie -> {
+                    exchange.getResponse().addCookie(ResponseCookie.from(config.name, cookie).build());
+                });
+                logger.info("Ejecutando el post gateway filter factory: " + config.message);
             }));
 
         });
