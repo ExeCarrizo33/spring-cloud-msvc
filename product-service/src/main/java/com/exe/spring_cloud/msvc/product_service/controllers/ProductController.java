077242ebc50561aac861b05f7dddbeb71015665a
Exe <exelcarrizo@gmail.com>
2025-03-21 14:34:17 -0300

    CRUD para products

--- product-service/src/main/java/com/exe/spring_cloud/msvc/product_service/controllers/ProductController.java
+++ product-service/src/main/java/com/exe/spring_cloud/msvc/product_service/controllers/ProductController.java
@@ -3,11 +3,9 @@ package com.exe.spring_cloud.msvc.product_service.controllers;
 import com.exe.spring_cloud.msvc.product_service.models.Product;
 import com.exe.spring_cloud.msvc.product_service.services.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -27,9 +25,9 @@ public class ProductController {
     @GetMapping("/{id}")
     public ResponseEntity<?> findById(@PathVariable Long id) throws InterruptedException {
 
-        if (id.equals(10L)) {
-            throw new IllegalStateException("No se puede cargar el producto");
-        }
+//        if (id.equals(10L)) {
+//            throw new IllegalStateException("No se puede cargar el producto");
+//        }
         if (id.equals(7L)) {
             TimeUnit.SECONDS.sleep(3L);
         }
@@ -37,6 +35,28 @@ public class ProductController {
         return productService.findById(id)
                 .map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<?> create(@RequestBody Product product) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(productService.save(product));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@PathVariable Long id, @RequestBody Product product) {
+        return productService.findById(id)
+                .map(productObj -> {
+                    productObj.setName(product.getName());
+                    productObj.setPrice(product.getPrice());
+                    productObj.setCreatedAt(product.getCreatedAt());
+                    return ResponseEntity.status(HttpStatus.CREATED).body(productService.save(productObj));
+                }).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        this.productService.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 
 
