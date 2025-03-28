b88d812353045c595e0f8c0cbb5a053e7389fb43
Exe <exelcarrizo@gmail.com>
2025-03-28 12:03:56 -0300

    Registro de los Claims Roles en el token JWT

--- gateway-server/src/main/java/com/exe/spring_cloud/msvc/gateway_server/security/SecurityConfig.java
+++ gateway-server/src/main/java/com/exe/spring_cloud/msvc/gateway_server/security/SecurityConfig.java
@@ -2,28 +2,50 @@ package com.exe.spring_cloud.msvc.gateway_server.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
 
-import static org.springframework.security.config.Customizer.withDefaults;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+import static org.springframework.security.config.Customizer.withDefaults; // Importa el método withDefaults de la clase Customizer
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
     SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) throws Exception {
-        return http.authorizeExchange(authz -> {
-            authz.pathMatchers("/authorized","/logout").permitAll()
-                    .pathMatchers(HttpMethod.GET,"/api/items","/api/products","/api/users").permitAll()
-                    .pathMatchers(HttpMethod.GET, "/api/items/{id}","/api/products/{id}","/api/users/{id}").hasAnyRole("ADMIN","USER")
-                    .pathMatchers("/api/items/**","/api/products/**","/api/users/**").hasRole("ADMIN")
-                    .anyExchange().authenticated();
-                }).cors(csrf -> csrf.disable())
-                .oauth2Login(withDefaults())
-                .oauth2Login(withDefaults())
-                .oauth2ResourceServer(withDefaults())
-                .build();
+        return http.authorizeExchange(authz -> { // Configura la autorización de intercambios
+            authz.pathMatchers("/authorized","/logout").permitAll() // Permite el acceso a las rutas /authorized y /logout a todos
+                    .pathMatchers(HttpMethod.GET,"/api/items","/api/products","/api/users").permitAll() // Permite el acceso GET a las rutas /api/items, /api/products y /api/users a todos
+                    .pathMatchers(HttpMethod.GET, "/api/items/{id}","/api/products/{id}","/api/users/{id}").hasAnyRole("ADMIN","USER") // Permite el acceso GET a las rutas con cualquier subruta a usuarios con roles ADMIN o USER
+                    .pathMatchers("/api/items/**","/api/products/**","/api/users/**").hasRole("ADMIN") // Permite el acceso a las rutas con cualquier subruta a usuarios con rol ADMIN
+                    .anyExchange().authenticated(); // Requiere autenticación para cualquier otro intercambio
+                }).cors(csrf -> csrf.disable()) // Deshabilita la protección CSRF
+                .oauth2Login(withDefaults()) // Habilita el inicio de sesión OAuth2 con la configuración predeterminada
+                .oauth2Client(withDefaults()) // Habilita el cliente OAuth2 con la configuración predeterminada
+                .oauth2ResourceServer(oAuth2 -> oAuth2.jwt(
+                        jwt -> jwt.jwtAuthenticationConverter(new Converter<Jwt, Mono<? extends AbstractAuthenticationToken>>() { // Configura el convertidor de autenticación JWT
+                            @Override
+                            public Mono<? extends AbstractAuthenticationToken> convert(Jwt source) { // Convierte un token JWT en un token de autenticación
+                                Collection<String> roles = source.getClaimAsStringList("roles"); // Obtiene los roles del token JWT
+                                Collection<GrantedAuthority> authorities = roles.stream() // Convierte los roles en una secuencia de autoridades
+                                        .map(SimpleGrantedAuthority::new) // Mapea los roles a una colección de autoridades
+                                        .collect(Collectors.toList());
+                                return Mono.just(new JwtAuthenticationToken(source,authorities)); // Devuelve un token de autenticación JWT
+                            }
+                        })
+                )) // Configura el servidor de recursos OAuth2 para usar JWT
+                .build(); // Construye y devuelve la cadena de filtros de seguridad
 
     }
 
--- oauth-service/src/main/java/com/exe/spring_cloud/msvc/oauth_service/security/SecurityConfig.java
+++ oauth-service/src/main/java/com/exe/spring_cloud/msvc/oauth_service/security/SecurityConfig.java
@@ -11,13 +11,17 @@ import org.springframework.core.annotation.Order;
 import org.springframework.http.MediaType;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.OAuth2Token;
 import org.springframework.security.oauth2.core.oidc.OidcScopes;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.server.authorization.OAuth2TokenType;
 import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
@@ -25,6 +29,8 @@ import org.springframework.security.oauth2.server.authorization.config.annotatio
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
 import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext;
+import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
@@ -34,7 +40,9 @@ import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
+import java.util.Objects;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Configuration
 public class SecurityConfig {
@@ -152,6 +160,21 @@ public class SecurityConfig {
     @Bean
     AuthorizationServerSettings authorizationServerSettings() {
         return AuthorizationServerSettings.builder().build();
+    }
+
+    /**
+     * Agregación de roles a los tokens JWT
+     */
+    @Bean
+    OAuth2TokenCustomizer<JwtEncodingContext> tokenCustomizer(){
+        return (context) -> {
+            if (Objects.equals(context.getTokenType().getValue(), OAuth2TokenType.ACCESS_TOKEN.getValue())) {
+                Authentication principal = context.getPrincipal();
+                context.getClaims().claim("roles", principal.getAuthorities().stream()
+                        .map(GrantedAuthority::getAuthority)
+                        .collect(Collectors.toList()));
+            }
+        };
     }
 
 }
