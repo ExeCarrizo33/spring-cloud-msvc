9f3f01de25894c1f248dc93587af6ad8ee31e980
Exe <exelcarrizo@gmail.com>
2025-04-09 19:19:58 -0300

    Arreglo de filtro de CircuitBreaker

--- gateway-server/src/main/java/com/exe/spring_cloud/msvc/gateway_server/GatewayServerApplication.java
+++ gateway-server/src/main/java/com/exe/spring_cloud/msvc/gateway_server/GatewayServerApplication.java
@@ -2,12 +2,36 @@ package com.exe.spring_cloud.msvc.gateway_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
+import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
+import static org.springframework.cloud.gateway.server.mvc.predicate.GatewayRequestPredicates.path;
+import static org.springframework.cloud.gateway.server.mvc.filter.LoadBalancerFilterFunctions.lb;
+import static org.springframework.cloud.gateway.server.mvc.filter.CircuitBreakerFilterFunctions.circuitBreaker;
+import static org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions.stripPrefix;
+
 
 @SpringBootApplication
 public class GatewayServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayServerApplication.class, args);
+    }
+
+    @Bean
+    RouterFunction<ServerResponse> routerConfig() {
+       return route("product-service").route(path("/api/products/**"), http())
+               .filter(lb("product-service"))
+               .filter(circuitBreaker(config -> config
+                       .setId("products")
+                       .setStatusCodes("500")
+                       .setFallbackPath("forward:/api/items/5")))
+               .before(stripPrefix(2))
+               .build();
+
     }
 
 }
